6c0e98ed2b59387dace774b014afbf905d2fd0b8
server <server@server1.yukkuriikouze.com>
2020-10-30 15:30:54 +0900

    ストップスケジューラー追加

--- items.py
+++ items.py
@@ -1,46 +1,78 @@
 #!/usr/bin/python3
 
-import psutil
-import pylxd
-import aiohttp
-import time
+import inspect
 import asyncio
-import netifaces as ni
 import os
 import socket
+import time
 
+import aiohttp
+import netifaces as ni
+import psutil
+import pylxd
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+# スケジューラー
+stop_machine_schedules = {}
+Schedule = AsyncIOScheduler()
+Schedule.start()
+
+
+# lxdクライアント
 client = pylxd.Client()
 
+
+def make_response_dict(
+    status=True,
+    reason="success",
+    details=""
+):
+    return {
+        "status": status,
+        "reason": reason,
+        "details": details
+    }
+
+
 async def launch_machine(
-        hostname       ,
-        imagealias      ,
-        imagefinger       ,
-        machinetype      ,
-        cpu              ,
-        memory              ,
-        storage               ,
-        srcport               ,
-        startcheck           ,
-        https           ,
-        httpstatus            ,
-        starttimeout       ,
-        startportassign
-    ):
-    aliases,fingerprint=get_all_image()
-    if ( imagealias == "" ) and ( imagefinger == "" ):
-        raise Exception("イメージが指定されていません")
-    elif imagealias !="" and len( [s for s in aliases if imagealias == s] ) == 0 :
-        raise Exception("イメージ名が異なっています")
-    elif imagefinger !="" and len( [s for s in fingerprint if s.startswith(imagefinger)] ) == 0 :
-        raise Exception("イメージ名が異なっています")
-    #マシンが無ければ新規作成
-    if None == get_machine(hostname):
-        #コンテナであれば
-        if machinetype =="container":
-            assign_port = scan_available_port( int( startportassign ) )
+    hostname,
+    imagealias,
+    imagefinger,
+    machinetype,
+    cpu,
+    memory,
+    storage,
+    srcport,
+    startcheck,
+    https,
+    httpstatus,
+    starttimeout,
+    startportassign
+):
+    # ローカルイメージ検索
+    aliases, fingerprint = get_all_image()
+    if (imagealias == "") and (imagefinger == ""):
+        # raise Exception("イメージが指定されていません")
+        return make_response_dict(False, "image_error", "イメージが指定されていません")
+
+    elif imagealias != "" and len([s for s in aliases if imagealias == s]) == 0:
+        # raise Exception("イメージ名が異なっています")
+        return make_response_dict(False, "image_error", "イメージエイリアスが異なっています")
+    elif imagefinger != "" and len([s for s in fingerprint if s.startswith(imagefinger)]) == 0:
+        # raise Exception("イメージ名が異なっています")
+        return make_response_dict(
+            False, "image_error", "イメージフィンガープリントが異なっています")
+
+    # マシンが無ければ新規作成
+    machine = get_machine(hostname)
+    assign_port = None
+    if None is machine:
+        # コンテナであれば
+        if machinetype == "container":
+            assign_port = scan_available_port(int(startportassign))
             """
             launch_container_machine(
-                hostname=hostname, 
+                hostname=hostname,
                 srcport=srcport ,
                 dstport=assign_port,
                 cpu=cpu,
@@ -51,21 +83,27 @@ async def launch_machine(
             """
             print(get_ip())
             pass
-        #それ以外は仮想マシン
+        # それ以外は仮想マシン
         else:
             pass
+    # マシンが存在場合
     else:
-        pass
-    #起動確認
+        machine.start()
+        if len(get_machine_used_port(hostname)) != 0:
+            assign_port = get_machine_used_port(hostname)[0]
+
+    # 起動確認
+    try_url=""
+    
     if int(startcheck):
         await get_html("https://127.0.0.1:9090")
     else:
         return
 
 
-async def wait_get_html(url,status,time_out):
-    start_time=time.time()
-    while start_time-time.time() < time_out :
+async def wait_get_html(url, status, time_out):
+    start_time = time.time()
+    while start_time - time.time() < time_out:
         await asyncio.sleep(1)
         result = await get_html(url)
         if result == status:
@@ -78,36 +116,44 @@ async def get_html(url):
         try:
             async with session.get(url) as resp:
                 return int(resp.status)
-        except aiohttp.client_exceptions.ClientConnectorError :
+        except aiohttp.client_exceptions.ClientConnectorError:
             return 0
 
+
 def get_all_image():
-    aliases     =   []
-    fingerprint =   []
+    aliases = []
+    fingerprint = []
     for image in client.images.all():
-        if len( image.aliases ) != 0:
+        if len(image.aliases) != 0:
             for i in image.aliases:
                 if "name" in i:
-                    aliases.append( i["name"] )
-        fingerprint.append( image.fingerprint )
-    return aliases,fingerprint
+                    aliases.append(i["name"])
+        fingerprint.append(image.fingerprint)
+    return aliases, fingerprint
+
 
 def get_all_machine_name():
-    A=[]
-    B=[container.name for container in client.containers.all()]
-    C=[virtual_machine.name for virtual_machine in client.virtual_machines.all()]
+    A = []
+    B = [container.name for container in client.containers.all()]
+    C = [virtual_machine.name for virtual_machine in client.virtual_machines.all()]
     A.extend(B)
     A.extend(C)
     return A
-        
 
 
-def launch_container_machine(hostname="", srcport="" ,dstport="",cpu="2",memory="4GB",fingerprint="",aliases=""):
-    image={}
-    if fingerprint!="":
-        image={"type": "image", "fingerprint": str(fingerprint)}
-    elif aliases!="":
-        image={"type": "image", "aliases": str(aliases)}
+def launch_container_machine(
+        hostname="",
+        srcport="",
+        dstport="",
+        cpu="2",
+        memory="4GB",
+        fingerprint="",
+        aliases=""):
+    image = {}
+    if fingerprint != "":
+        image = {"type": "image", "fingerprint": str(fingerprint)}
+    elif aliases != "":
+        image = {"type": "image", "aliases": str(aliases)}
     config = {
         "name": str(hostname),
         "source": str(image),
@@ -115,8 +161,8 @@ def launch_container_machine(hostname="", srcport="" ,dstport="",cpu="2",memory=
         "devices": {
             "vscode-port": {
                 "bind": "host",
-                "connect": "tcp:127.0.0.1:" +   str(srcport),
-                "listen": "tcp:0.0.0.0:"    +   str(dstport),
+                "connect": "tcp:127.0.0.1:" + str(srcport),
+                "listen": "tcp:0.0.0.0:" + str(dstport),
                 "type": "proxy"}
         }
     }
@@ -156,11 +202,54 @@ def get_machine(name):
     return machine
 
 
-def stop_machine(name):
-    machine = get_machine(name)
+# マシンストップスケジューラー
+def add_stop_machine_schedule(machine_name, delay):
+    if delay < 5:
+        delay = 5
+    try:
+        schedule_job = Schedule.add_job(
+            lambda: stop_machine(machine_name, call_from_scheduler=True),
+            trigger='interval',
+            seconds=delay,
+            id=machine_name + "-stop-scheduled"
+        )
+    except BaseException:
+        return make_response_dict(False, "existing_job")
+    return make_response_dict()
+
+
+def remove_stop_machine_schedule(machine_name):
+    try:
+        Schedule.remove_job(machine_name + "-stop-scheduled")
+    except BaseException:
+        return make_response_dict(False, "job_not_found")
+    return make_response_dict()
+
+
+def get_stop_machine_schedule():
+    schedules = []
+    for job in Schedule.get_jobs():
+        schedules.append(
+            {
+                "Name": str(job.id),
+                "Run Frequency": str(job.trigger),
+                "Next Run": str(job.next_run_time)
+            }
+        )
+    return make_response_dict(details=schedules)
+
+
+def stop_machine(machine_name, call_from_scheduler=False):
+    if call_from_scheduler:
+        remove_stop_machine_schedule(machine_name)
+    machine = get_machine(machine_name)
     if machine is None:
         return False
-    machine.stop()
+    try:
+        machine.stop()
+    except BaseException:
+        return False
+    return True
 
 
 def start_machine(name):
@@ -173,6 +262,20 @@ def start_machine(name):
 def delete_machine(name):
     print(get_machine(name))
 
+
+def get_machine_used_port(machine_name):
+    machine = get_machine(machine_name)
+    if machine is None:
+        return []
+    used_ports = []
+    for key in machine.devices:
+        if "type" in machine.devices[key]:
+            if machine.devices[key]["type"] == "proxy":
+                used_ports.append(
+                    int(machine.devices[key]["connect"].split(":")[-1]))
+    return used_ports
+
+
 def get_used_port():
     """
     現在利用中のportと割り当て済みのport一覧作成
@@ -183,17 +286,18 @@ def get_used_port():
         for key in machine.devices:
             if "type" in machine.devices[key]:
                 if machine.devices[key]["type"] == "proxy":
-                    used_ports.append( int( machine.devices[key]["connect"].split(":")[-1] ) ) 
+                    used_ports.append(
+                        int(machine.devices[key]["connect"].split(":")[-1]))
 
     for machine in client.virtual_machines.all():
         for key in machine.devices:
             if "type" in machine.devices[key]:
                 if machine.devices[key]["type"] == "proxy":
-                    used_ports.append( int( machine.devices[key]["connect"].split(":")[-1] ) ) 
-    used_ports=list(set(used_ports))
-    used_ports.sort()
+                    used_ports.append(
+                        int(machine.devices[key]["connect"].split(":")[-1]))
+    used_ports = sorted(set(used_ports))
     return used_ports
-    
+
 
 def check_port_available(port):
     used_ports = get_used_port()
@@ -201,6 +305,7 @@ def check_port_available(port):
         return False
     else:
         return True
+
 
 def scan_available_port(start_port):
     port_offset = 0
@@ -210,9 +315,10 @@ def scan_available_port(start_port):
         if port_candidate in used_ports:
             port_offset += 1
         else:
-            return port_candidate 
+            return port_candidate
         if port_candidate > 65535:
             raise Exception("利用可能なport上限を超えました")
+
 
 def get_ip() -> list:
     if os.name == "nt":
@@ -270,6 +376,8 @@ def make_csv(
                 port_offset += 1
     with open(out_file_path, mode='w') as f:
         f.write('\n'.join(file_meta))
+
+
 def make_csv_from_str(
         in_file_str="",
         start_port=0,
@@ -299,16 +407,18 @@ def make_csv_from_str(
                 port_offset += 1
     return '\n'.join(file_meta)
 
-def get_machine_file(machine_name,file_path,local_path):
-    machine=get_machine(machine_name)
-    file_meta = machine.files.recursive_get(file_path,local_path)
+
+def get_machine_file(machine_name, file_path, local_path):
+    machine = get_machine(machine_name)
+    file_meta = machine.files.recursive_get(file_path, local_path)
 
     return file_meta
 
+
 if __name__ == "__main__":
-    print(pylxd.__file__)
-    print(get_machine_file("test-vm","/hoge","temp/hoge"))
-    
+    if "0" :
+        print(get_machine_used_port("rin331"))
+
 
 """
 make_csv(
